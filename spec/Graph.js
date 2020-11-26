10afc802bad99385a524e2088bfde6d25334211e
Henri Bergius <henri.bergius@iki.fi>
2020-11-26 12:24:52 +0100

    Fix description

--- spec/Graph.js
+++ spec/Graph.js
@@ -2,7 +2,8 @@ const chai = require('chai');
 const noflo = require('noflo');
 const Tester = require('../lib/wrapper');
 
-describe('Wrapping a graph from library', () => {
+describe('Wrapping a Graph object', () => {
+  // Create a noflo.Graph object (could also be loaded from JSON/FBP)
   const graph = new noflo.Graph('Test Graph');
   graph.addNode('kick', 'core/Kick');
   graph.addNode('repeat', 'core/RepeatAsync');
@@ -12,6 +13,7 @@ describe('Wrapping a graph from library', () => {
   }, 'kick', 'data');
   graph.addInport('start', 'kick', 'in');
   graph.addOutport('result', 'repeat', 'out');
+  // Start the wrapper
   const t = new Tester(graph);
   before((done) => t.start(done));
   it('should use the graph directly', () => {
